7b1d6adb84264df2e860a4880c24c6bf06376026
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2015-12-15 13:55:58 -0800

    Working on UD for organizers

--- public/js/app.js
+++ public/js/app.js
@@ -32,7 +32,67 @@ $(document).ready(function() {
   $('#projects').on('click', '.edit-project', handleEditProjectClick);
 
   $('#projects').on('click', '.put-project', handleSaveChangesClick);
+
+  $('#editOrganizersModal').on('click', '.delete-organizer', handleDeleteOrganizerClick);
+
+  $('#editOrganizersModal').on('submit', 'form', handleUpdateOrganizer);
 });
+
+// takes an array of organizers and generates an EDIT form for them
+// we want to have both the projectId and organizerId available
+function generateEditOrganizersModalHtml(organizers, projectId) {
+  var html = '';
+  organizers.forEach(function(organizer) {
+    html += '<form class="form-inline" id="' + projectId  + '"' +
+            '  <div class="form-group">' +
+            '    <input type="text" class="form-control organizer-firstName" value="' + organizer.firstName + '">' +
+            '  </div>'+
+            '  <div class="form-group">' +
+            '    <input type="text" class="form-control organizer-lastName" value="' + organizer.lastName + '">' +
+            '  </div>'+
+            '  <div class="form-group">' +
+            '    <input type="text" class="form-control organizer-email" value="' + organizer.email + '">' +
+            '  </div>'+
+            '  <div class="form-group">' +
+            '    <button class="btn btn-danger delete-organizer" data-organizer-id="' + organizer._id + '">x</button>' +
+            '  </div>'+
+            '  <div class="form-group">' +
+            '    <button type="submit" class="btn btn-success save-organizer" data-organizer-id="' + organizer._id + '">save</span></button>' +
+            '  </div>'+
+            '</form>';
+  });
+
+  return html;
+}
+
+function handleDeleteOrganizerClick(e) {
+  e.preventDefault();
+  var organizerId = $(this).data('organizer-id');
+  var projectId = $(this).closest('form').attr('id');
+  var $thisOrganizer = $(this);
+  var requestUrl = ('/api/projects/' + projectId + '/organizers/' + organizerId);
+  console.log('DELETE ', requestUrl);
+  $.ajax({
+    method: 'DELETE',
+    url: requestUrl,
+    success: function(data) {
+      $thisOrganizer.closest('form').remove();
+      updateOrganizersList(projectId);
+    }
+  });
+}
+
+// get the organizers again (now that one is gone) and then we'll fix the <li> on the package
+function updateOrganizersList(projectId) {
+  $.get('/api/projects/' + projectId + '/organizers').success(function(someProjects) {
+    console.log('replacement projects', someProjects);
+    // build a new li
+    var replacementLi = buildSongsHtml(someAlbums);
+    // now replace the <li> with the organizers on it.
+    var $originalLi = $('[data-project-id=' + projectId + '] .organizers-list');
+    $originalLi.replaceWith(replacementLi);
+  });
+}
 
 function getProjectRowById(id) {
   return $('[data-project-id=' + id + ']');
@@ -130,7 +190,18 @@ function handleNewOrganizerSubmit(e) {
     });
 }
 
-
+function buildOrganizersHtml(organizers) {
+  var organizerText = "    &ndash; ";
+  organizers.forEach(function(organizer) {
+    organizerText = organizerText + "(" + organizer.firstName + ") " + organizer.lastName + ") " + organizer.email + " &ndash; ";
+  });
+  var organizersHtml  =
+   "                      <li class='list-group-item organizers-list'>" +
+   "                        <h4 class='inline-header'>Organizers:</h4>" +
+   "                         <span>" + organizerText + "</span>" +
+   "                      </li>";
+  return organizersHtml;
+}
 
 // this function takes a single project and renders it to the page
 function renderProject(taco) {
